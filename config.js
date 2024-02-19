af9af308c94d696255ca0821354c0a8c3f423959
Rishabh Suryavanshi <rishabhsuryavanshi@Rishabhs-MacBook-Air.local>
2024-02-19 17:30:25 +0530

    first

---
+++ config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = "rishabh"
+
+module.exports = JWT_SECRET
---
+++ db.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("" +
+"replicaSet=rs"
+)
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 3
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+})
+
+const accountSchema = new mongoose.Schema({
+    userId:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance:{
+        type: Number,
+        required: true
+    }
+
+})
+
+const User = mongoose.model("User", userSchema)
+const Account  = mongoose.model("Account", accountSchema)
+
+module.exports ={
+    User,
+    Account
+}
